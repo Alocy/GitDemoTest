3a292a14d7675ada2894b82a50d2fdbbcc6d4c4c
Alocy <958687112@qq.com>
2016-03-23 10:18:00 +0800

    first blood

---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by rimi on 16/3/23.
+//  Copyright © 2016年 zhangshiiliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by rimi on 16/3/23.
+//  Copyright © 2016年 zhangshiiliang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor orangeColor];
+    NSLog(@"wojiufuckle");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
